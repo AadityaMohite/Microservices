8d0c95b73054a0db7546082b0cb0ed83cf0debbf
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2026-03-30 20:15:24 +0530

    order service add

---
+++ OrderServiceMicroservice1Application.java
@@ -0,0 +1,15 @@
+package com.Aadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class OrderServiceMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceMicroservice1Application.class, args);
+	}
+
+}
